bac0bc9eebb97f07a181e24b26fa28fb5935fd71
rui_er <qz-cqy@outlook.com>
2023-01-09 19:31:41 +0800

    upload files

---
+++ README.md
@@ -0,0 +1,3 @@
+# [qz-cqy](https://github.com/qz-cqy) / [THUHS-C1916-Hello-2023-Random-Selection](https://github.com/qz-cqy/THUHS-C1916-Hello-2023-Random-Selection)
+
+这是清华附中 C1916 班 2023 元旦联欢会的随机抽号程序，维护者的学号是 C191603 / G221201。代码仓库使用 [MIT 许可协议](https://github.com/qz-cqy/THUHS-C1916-Hello-2023-Random-Selection/blob/main/LICENSE)。
---
+++ script/main.js
@@ -0,0 +1,130 @@
+function getQueryVariable(variable) {
+    let query = window.location.search.substring(1);
+    let vars = query.split("&");
+    for (let i = 0; i < vars.length; i++) {
+        let pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return '-1';
+}
+function choice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+let arr = new Array();
+let vis = new Array();
+let weight = new Array();
+let chosen = 0;
+function load() {
+    if(arr.length == 0) {
+        let html = '';
+        html += `<p>还没有数据。</p>`;
+        document.getElementById('show-weight').innerHTML = html;
+    }
+    else {
+        let html = '';
+        let n = arr.length;
+        for(let i = 0; i < n; i++) {
+            html += `<div class="student ${vis[i] ? "unavailable" : "available"}">${arr[i]}(${weight[i]})</div>`;
+            if(i % 10 == 9 && i != n - 1) html += `<br>`;
+        }
+        document.getElementById('show-weight').innerHTML = html;
+    }
+}
+function init() {
+    let L = parseInt(document.getElementById('range-minimum').value);
+    let R = parseInt(document.getElementById('range-maximum').value);
+    if (L > R) {
+        let html = '';
+        html += `<p style='color: red;'>请检查你的输入！</p>`;
+        document.getElementById('result-init').innerHTML = html;
+    }
+    else if (R - L + 1 > 100) {
+        let html = '';
+        html += `<p style='color: red;'>范围过大！（不能超过 100）</p>`;
+        document.getElementById('result-init').innerHTML = html;
+    }
+    else {
+        arr = new Array();
+        vis = new Array();
+        weight = new Array();
+        for (let i = L; i <= R; i++) {
+            arr.push(i);
+            vis.push(0);
+            weight.push(1);
+        }
+        let html = '';
+        html += `<p style='color: blue;'>修改成功！已修改为 ${L}~${R}</p>`;
+        document.getElementById('result-init').innerHTML = html;
+        load();
+    }
+}
+function generate() {
+    let tmp = new Array();
+    let n = arr.length;
+    for(let i = 0; i < n; i++) {
+        if(!vis[i]) {
+            for(let j = 0; j < weight[i]; j++) {
+                tmp.push(i);
+            }
+        }
+    }
+    if(tmp.length == 0) {
+        let html = '';
+        html += `<p style='color: red;'>没有可抽取的！</p>`;
+        chosen = 0;
+        document.getElementById('regenerate').disabled = true;
+        document.getElementById("display-area").innerHTML = html;
+        document.getElementById('regenerate').disabled = false;
+    }
+    else {
+        let lucky = choice(tmp);
+        let html = '';
+        html += `幸运的是：<div class='lucky'>${arr[lucky]}</div>号！`;
+        chosen = 0;
+        document.getElementById('regenerate').disabled = true;
+        let intv = setInterval(function () {
+            let lucky_t = choice(tmp);
+            let html_t = '';
+            html_t += `幸运的是：<div class='lucky'>${arr[lucky_t]}</div>号！`;
+            document.getElementById("display-area").innerHTML = html_t;
+        }, 100);
+        setTimeout(function () {
+            clearInterval(intv);
+            chosen = lucky;
+            document.getElementById("display-area").innerHTML = html;
+            document.getElementById('regenerate').disabled = false;
+        }, 2000);
+    }
+}
+function mark() {
+    if(!chosen) return;
+    vis[chosen] = 1;
+    load();
+}
+function modify() {
+    let id = parseInt(document.getElementById('student-id').value);
+    let ok = parseInt(document.getElementById('available').value);
+    let wt = parseInt(document.getElementById('new-weight').value);
+    if(ok != 0 && ok != 1) return;
+    if(wt < 0 || wt > 10) return;
+    let n = arr.length;
+    for(let i = 0; i < n; i++) {
+        if(arr[i] == id) {
+            vis[i] = ok ^ 1;
+            weight[i] = wt;
+        }
+    }
+    load();
+}
+(function () {
+    let date = new Date();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    if (month == 12 && day == 13) {
+        setStyle("html", "-webkit-filter", "grayscale(100%)");
+        setStyle("html", "-moz-filter", "grayscale(100%)");
+        setStyle("html", "-ms-filter", "grayscale(100%)");
+        setStyle("html", "-o-filter", "grayscale(100%)");
+        setStyle("html", "filter", "grayscale(100%)");
+    }
+})();
